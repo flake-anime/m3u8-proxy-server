7e36db3557d55f4d5bcf452e2e048a25aaf135d7
Irony00 <r.rahul.developer@gmail.com>
2022-02-19 09:04:38 +0530

    minor bug fix

--- proxy_server.mjs
+++ proxy_server.mjs
@@ -1,13 +1,14 @@
 // https://www28.gogocdn.club/www04/DguGWt0YYiBxIm6AqsWnfA/1645298559/e7b499c11b82dfdaa72dc10c08c31757/ep.1.1634310704.720.m3u8
 
 import express from 'express';
-import fetch from 'node-fetch';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import got from 'got';
 import stream from 'stream';
+import cors from 'cors';
 
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 3000
 const Transform = stream.Transform;
 
@@ -27,7 +28,7 @@ app.get('/fetch_m3u8', async (req, res) => {
 		const regex = /([a-z0-9./-]*.ts)/gm
 		const m3u8_filtered = Buffer.from(
 			data.toString()
-			.replace(regex, `http://localhost:3000/fetch_ts?url=${base_url}/$1`)
+			.replace(regex, `/fetch_ts?url=${base_url}/$1`)
 		)
 		this.push(m3u8_filtered)
 		// console.log(m3u8_filtered.toString())
